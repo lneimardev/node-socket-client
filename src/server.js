038861f63f0c1277d9c88f0394fc1c56ce985da6
Neimar Locatelli <lneimar.dev@gmail.com>
2022-08-02 22:40:17 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const { io } = require("socket.io-client");
+
+const socket = io("ws://localhost:3000");
+
+socket.on("canal1", (arg) => {
+    console.log(`canal1: ${arg}`);
+});
+
+socket.on("canal2", (arg) => {
+    console.log(`canal2: ${arg}`);
+});
+
+
